97fc03e0116189793b417497ab12b17e589c06ae
Fyonietz <expduta@gmail.com>
2025-07-06 22:47:48 +0700

    just minor changes

--- src/Backend/backend.cpp
+++ src/Backend/backend.cpp
@@ -266,7 +266,8 @@ std::vector<std::string> find_all_json_files(const std::string &root_dir){
 
 void cmd(std::string &command){
     std::unordered_map<std::string,std::string> command_key={
-        "::sys::terminate","shutdown -s "
+        "::sys::terminate","shutdown -s ",
+        "::sys::sleep","sleep"
     };
     //command
     std::string command_after_parser = command.substr(command.find('=')+1);
